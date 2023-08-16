cb1fe88908cb983a1d6923de738dc59b42e81b20
Achmad Lintang W <lintang.special@gmail.com>
2023-08-16 19:23:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#fcfaf8',
+        secondary: '#70c600',
+        accent: '#f3eee9',
+        neutral: '#292828'
+      },
+  fontFamily: {
+    'sans': "'IBM Plex Sans', sans-serif",
+}
+
+    },
+  },
+  plugins: [],
+}
+
